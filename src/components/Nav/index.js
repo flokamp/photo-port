2e461091b5953cd444f8ebfe86216e3f0e8bc176
flokamp <74511633+flokamp@users.noreply.github.com>
2021-05-03 19:01:51 -0500

    create navbar component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function categorySelected(name) {
+	console.log(`${name} clicked`);
+}
+
+{
+	categories.map((category) => (
+		<li className='mx-1' key={category.name}>
+			<span onClick={() => categorySelected(category.name)}>
+				{category.name}
+			</span>
+		</li>
+	));
+}
+
+function Nav() {
+	return (
+		<header>
+			<h2>
+				<a href='/'>
+					<span role='img' aria-label='camera'>
+						{" "}
+						📸
+					</span>{" "}
+					Oh Snap!
+				</a>
+			</h2>
+			<nav>
+				<ul className='flex-row'>
+					<li className='mx-2'>
+						<a href='#about'>About me</a>
+					</li>
+					<li>
+						<span>Contact</span>
+					</li>
+					{categories.map((category) => (
+						<li className='mx-1' key={category.name}>
+							<span onClick={categorySelected}>{category.name}</span>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default Nav;
