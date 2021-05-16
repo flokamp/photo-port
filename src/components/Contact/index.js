d23b528f1c416fad2d772f3bf0461b6ef23fa283
flokamp <74511633+flokamp@users.noreply.github.com>
2021-05-16 17:34:09 -0500

    added conditional rendering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
 	const [formState, setFormState] = useState({
@@ -7,10 +8,30 @@ function ContactForm() {
 		message: "",
 	});
 
+	const [errorMessage, setErrorMessage] = useState("");
+
 	const { name, email, message } = formState;
 
 	function handleChange(e) {
-		setFormState({ ...formState, [e.target.name]: e.target.value });
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			console.log(isValid);
+			if (!isValid) {
+				setErrorMessage("Your email is invalid.");
+			} else {
+				setErrorMessage("");
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage("");
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({ ...formState, [e.target.name]: e.target.value });
+		}
 	}
 
 	function handleSubmit(e) {
@@ -50,6 +71,11 @@ function ContactForm() {
 						rows='5'
 					/>
 				</div>
+				{errorMessage && (
+					<div>
+						<p className='error-text'>{errorMessage}</p>
+					</div>
+				)}
 				<button type='submit'>Submit</button>
 			</form>
 		</section>
